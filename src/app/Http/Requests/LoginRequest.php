40886adc129576ae85292a807f0894e708e1ec56
mdotplus <sun.eat.summer@gmail.com>
2024-09-13 14:55:19 +0900

    LoginRequestの作成

---
+++ src/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email', 'unique:users,email', 'string', 'max:191'],
+            'password' => ['required', 'min:8', 'max:191'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスの形式で入力してください',
+            'email.string' => '適切なメールアドレスを入力してください',
+            'email.max' => 'メールアドレスは191文字以内で入力してください',
+            'password.required' => 'パスワードを入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.max' => 'パスワードは191文字以内で入力してください',
+        ];
+    }
+}
